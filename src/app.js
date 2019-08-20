0fa39f6e1137326cdbd808540b874f4d4943710e
Oscar Broch <brochj@gmail.com>
2019-08-20 11:56:39 -0300

    Configurando Sequelize

--- src/app.js
+++ src/app.js
@@ -2,20 +2,19 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-  constructor(){
+  constructor() {
     this.server = express();
     this.middlewares();
     this.routes();
   }
 
-  middlewares(){
+  middlewares() {
     this.server.use(express.json()); // agora o projeto aceita reqs no formato JSON
   }
 
-  routes(){
-    this.server.use(routes) // rotas tmbm funcionam como middlewares
+  routes() {
+    this.server.use(routes); // rotas tmbm funcionam como middlewares
   }
-
 }
 
-export default new App().server; //exportando uma nova instancia de App()
+export default new App().server; // exportando uma nova instancia de App()
--- src/routes.js
+++ src/routes.js
@@ -3,25 +3,7 @@ import { Router } from 'express'; // Importa apenas o Router, e nao o express in
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({msg: 'Hello World'})
-})
+  return res.json({ msg: 'Hello World' });
+});
 
 export default routes;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
