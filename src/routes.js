57900b2bea1f5ce61153c0b2501644332d5a2ee7
Oscar Broch <brochj@gmail.com>
2019-08-20 15:09:50 -0300

    Cadastro de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express'; // Importa apenas o Router, e nao o express inteiro
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Oscar Broch',
-    email: 'brochj@gmail.com',
-    password_hash: 'fddsf3423432df3r',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
