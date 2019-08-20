76730ddbcf61337d8b16a9753a839337bb313b16
Oscar Broch <brochj@gmail.com>
2019-08-20 16:04:15 -0300

    Gerando hash da senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,11 +8,20 @@ class User extends Model {
         // chamando o metodo init() da classe Model
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // esse campo nunca vai existir no DB
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
       { sequelize }
     );
+    // antes de qulqr usuario ser salvo/alterado, roda-se esse o que ta dentro desse hook
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
