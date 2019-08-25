b752de9e701fe041208675f6d67cb3d59e068dff
Oscar Broch <brochj@gmail.com>
2019-08-25 16:19:08 -0300

    Tratamento de excecoes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
+
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(express.json()); // agora o projeto aceita reqs no formato JSON
     // express.static = eh pra servir arquivo estaticos, como imgs,css,html
     this.server.use(
@@ -21,6 +34,14 @@ class App {
 
   routes() {
     this.server.use(routes); // rotas tmbm funcionam como middlewares
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://bf49497bb6a14042a83f33889863e2f0@sentry.io/1541186',
+};
