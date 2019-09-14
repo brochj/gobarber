0264502fb6c03c2ba4971e2fa37963fc7b28c961
Oscar Broch <brochj@gmail.com>
2019-09-14 18:27:57 -0300

    Estilos Globais

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 import history from './services/history';
 
@@ -8,6 +10,7 @@ export default function src() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
