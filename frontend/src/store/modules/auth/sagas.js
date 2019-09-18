ebcad8cfdd2bd11934d39a795ce080860266fb81
Oscar Broch <brochj@gmail.com>
2019-09-18 16:36:22 -0300

    Loading de autenticacao

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -2,26 +2,30 @@ import { call, put, all, takeLatest } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é prestador');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuário não é prestador');
+      return;
+    }
+    // put é para disparar actions
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (eer) {
+    yield put(signFailure());
   }
-  // put é para disparar actions
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 // all é pra ficar ouvindo as actions
