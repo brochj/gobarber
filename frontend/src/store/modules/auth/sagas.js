092b531679bda96f3c9553bcfd1d594a6f515e36
Oscar Broch <brochj@gmail.com>
2019-09-18 16:48:07 -0300

    Exibindo toast

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,4 +1,6 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
 import api from '~/services/api';
 import history from '~/services/history';
 
@@ -16,7 +18,8 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
+      yield put(signFailure());
       return;
     }
     // put é para disparar actions
@@ -24,6 +27,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (eer) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
