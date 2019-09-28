c11b61d397f166b8c9169ed45e905ce648183500
Oscar Broch <brochj@gmail.com>
2019-09-28 09:57:21 -0300

    Pagina de perfil

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Digite sua nova senha"
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Digite sua nova senha novamente"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair do goBarber</button>
+    </Container>
+  );
 }
