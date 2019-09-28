db89ac5b0bb2536873c4307b1526f46d80fd3e05
Oscar Broch <brochj@gmail.com>
2019-09-28 11:48:00 -0300

    Dados do Header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import Notifications from '~/components/Notifications'
+import Notifications from '~/components/Notifications';
 
 import logo from '~/assets/logo-purple.svg';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -16,14 +18,17 @@ export default function Header() {
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
         <aside>
-        <Notifications />
+          <Notifications />
           <Profile>
             <div>
-              <strong>Oscar Broch</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.io.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.io.png'
+              }
               alt="avatar"
             />
           </Profile>
