9d049e06e1d51265cbe10ef0e83b4288a4f45e59
Oscar Broch <brochj@gmail.com>
2019-10-05 17:37:47 -0300

    Atualização de perfil

--- mobile/src/components/Appointment/styles.js
+++ mobile/src/components/Appointment/styles.js
@@ -6,6 +6,7 @@ export const Container = styled.View`
   border-radius: 4px;
   background: #fff;
 
+  opacity: ${props => (props.past ? 0.6 : 1)};
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,16 +1,127 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import Background from '~/components/Background';
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Form,
+  SubmitButton,
+  Separator,
+  FormInput,
+} from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+  const profile = useSelector(state => state.user.profile);
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+  const [password, setPassword] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
+  function handleUpdate() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      })
+    );
+  }
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const oldPasswordRef = useRef();
+  const confirmPasswordRef = useRef();
+
   return (
     <Background>
       <Container>
-        <Text>Profile</Text>
+        <Title>Meu Perfil</Title>
       </Container>
+
+      <Form>
+        <FormInput
+          icon="person-outline"
+          autoCorrect={false}
+          autoFocus
+          blurOnSubmit={false}
+          placeholder="Nome Completo"
+          returnKeyType="next"
+          value={name}
+          onChangeText={text => setName(text)}
+          onSubmitEditing={() => emailRef.current.focus()}
+        />
+        <FormInput
+          ref={emailRef}
+          icon="mail-outline"
+          autoCorrect={false}
+          autoCapitalize="none"
+          blurOnSubmit={false}
+          placeholder="Digite seu email"
+          returnKeyType="next"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          onSubmitEditing={() => oldPasswordRef.current.focus()}
+        />
+
+        <Separator />
+
+        <FormInput
+          ref={oldPasswordRef}
+          icon="lock-outline"
+          secureTextEntry
+          maxLength={30}
+          blurOnSubmit={false}
+          placeholder="Sua senha atual"
+          value={oldPassword}
+          returnKeyType="next"
+          onChangeText={text => setOldPassword(text)}
+          onSubmitEditing={() => passwordRef.current.focus()}
+        />
+        <FormInput
+          ref={passwordRef}
+          icon="lock-outline"
+          secureTextEntry
+          maxLength={30}
+          blurOnSubmit={false}
+          placeholder="Sua nova senha"
+          value={password}
+          returnKeyType="next"
+          onChangeText={text => setPassword(text)}
+          onSubmitEditing={() => confirmPasswordRef.current.focus()}
+        />
+        <FormInput
+          ref={confirmPasswordRef}
+          icon="lock-outline"
+          secureTextEntry
+          maxLength={30}
+          placeholder="Confirmação de senha"
+          value={confirmPassword}
+          returnKeyType="send"
+          onChangeText={text => setConfirmPassword(text)}
+          onSubmitEditing={handleUpdate}
+        />
+        <SubmitButton loading={loading} onPress={handleUpdate}>
+          Atualizar Perfil
+        </SubmitButton>
+      </Form>
     </Background>
   );
 }
